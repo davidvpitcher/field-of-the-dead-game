45427fb28e50e70a3340fbf6747b0d4d4d6f0b09
David Pitcher <114188266+davidvpitcher@users.noreply.github.com>
2023-08-20 16:53:59 -0700

    Update ItemGuide.cs

--- Scripts/ItemGuide.cs
+++ Scripts/ItemGuide.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item", order = 1)]
 public class ItemGuide : ScriptableObject
 {
-
     public string itemName;
     public GameObject prefab;
-    public GameObject menuprefab;
+    public GameObject menuPrefab;
 }
